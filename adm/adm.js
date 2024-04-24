7d0cf7df76188ef64b6524d91be62fb6a2359cbe
Julinho <juliorezende311@gmail.com>
2024-04-24 11:33:26 -0300

    html login e logo

--- adm/adm.js
+++ adm/adm.js
@@ -1,10 +1,6 @@
 function mostrarValores() {
-    // Captura os valores dos campos de input
     var cfess = document.getElementById("cfess").value;
     var nome = document.getElementById("nome").value;
-
-    // Exibe um alert com os valores capturados
-    alert("Valor 1: " + cfess + "\nValor 2: " + nome);
 }
 
-`https://cfess-br.implanta.net.br/visaonacional/api/nacional/registros-nacionais?pageNumber=1&pageSize=50&ignorePagination=false&idRegistroNacional=${cfess}&nome=${nome}`
+// `https://cfess-br.implanta.net.br/visaonacional/api/nacional/registros-nacionais?pageNumber=1&pageSize=50&ignorePagination=false&idRegistroNacional=${cfess}&nome=${nome}`
